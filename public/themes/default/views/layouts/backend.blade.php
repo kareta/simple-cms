9a213e98f5d67101054bc888f5d490cfb4b5cbdc
Kareta Victor <karetavictor@gmail.com>
2016-04-30 19:51:12 +0300

    Add styles to default theme

--- public/themes/default/views/layouts/backend.blade.php
+++ public/themes/default/views/layouts/backend.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <title></title>
+    <link rel="stylesheet" href="/laravel-cms/public/themes/default/assets/css/backend.css">
 </head>
 <body>
     <h1>The CMS</h1>
