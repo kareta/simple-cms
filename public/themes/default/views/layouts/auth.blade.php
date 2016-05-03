7cf93a3456a8c31d7e61a3b70f7fa1ef585e03fc
Kareta Victor <karetavictor@gmail.com>
2016-05-03 10:11:20 +0300

    Add login errors checking

--- public/themes/default/views/layouts/auth.blade.php
+++ public/themes/default/views/layouts/auth.blade.php
@@ -8,11 +8,24 @@
     <div class="row vertical-center">
         <div class="col-md-4"></div>
         <div class="col-md-4">
-            <div class="panel panel-default">
+            <div class="panel panel-{{$errors->any() ? 'danger' : 'default'}}">
                 <div class="panel-heading">
                     <h2 class="panel-title">@yield('heading')</h2>
                 </div>
-                <div class="panel-body">@yield('content')</div>
+                <div class="panel-body">
+                    @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <strong>We found some errors</strong>
+                        </div>
+
+                        <ul>
+                            @foreach($errors->all() as $error)
+                                <li> {{ $error }} </li>
+                            @endforeach
+                        </ul>
+                    @endif
+                    @yield('content')
+                </div>
             </div>
         </div>
         <div class="col-md-4"></div>
