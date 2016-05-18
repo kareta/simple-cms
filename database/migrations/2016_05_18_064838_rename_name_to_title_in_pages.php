6f40122720ee6d78135f661d312384524068631f
Kareta Victor <karetavictor@gmail.com>
2016-05-18 14:25:19 +0300

    Create page model

---
+++ database/migrations/2016_05_18_064838_rename_name_to_title_in_pages.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RenameNameToTitleInPages extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('pages', function (Blueprint $table) {
+            $table->renameColumn('name', 'title');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('pages', function (Blueprint $table) {
+            $table->renameColumn('title', 'name');
+        });
+    }
+}
