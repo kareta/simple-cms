ce348e986b40ad355fde7b421b77adf8e1b2eafb
Kareta Victor <karetavictor@gmail.com>
2016-05-30 21:54:26 +0300

    Add posts table

---
+++ app/Post.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $fillable = ['author_id', 'title', 'slug', 'body', 'excerpt', 'published_at'];
+
+    protected $dates = ['published_at'];
+
+    public function setPublishedAtAttribute($value)
+    {
+        $this->attributes['published_at'] = $value ?: null;
+    }
+}
