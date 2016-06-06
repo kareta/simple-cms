ab4521831a301726578eec26fb6b8687d7bf3639
Kareta Victor <karetavictor@gmail.com>
2016-06-06 09:06:44 +0300

    Fix password reset my mail

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'vadimpetrovich9376@gmail.com', 'name' => 'vadimpetrovich9376'],
 
     /*
     |--------------------------------------------------------------------------
